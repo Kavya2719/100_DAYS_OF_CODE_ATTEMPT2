428682f511cd3cd4266b31d040f2da63d261dc2c
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-08-06 23:47:28 +0530

    Create DAY6_CC_SC31.cpp

---
+++ DAY6_CC_SC31.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+typedef long long ll;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   int sz;
+   cin >> sz;
+   
+   int ct[10]={0};
+   for(int i=0;i<sz;++i){
+     string s;
+     cin >> s;
+     for(int j=0;j<10;++j){
+       if(s[j]=='1') ct[j]++;
+     }
+   }
+   
+   int ans=0;
+   for(int i=0;i<10;++i){
+     if(ct[i]&1) ans++;
+   }
+   
+   cout << ans << "\n";
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=1;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
