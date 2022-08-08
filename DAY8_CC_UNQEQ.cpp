b615144ddeee18a1fbeb5064016fe2f1dff590c5
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-08-08 23:16:45 +0530

    Create DAY8_CC_UNQEQ.cpp

---
+++ DAY8_CC_UNQEQ.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+typedef long long ll;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   int sz;
+   cin >> sz;
+   
+   if(sz%4){no; return;}
+   yes;
+   
+   for(int i=sz;i>3*sz/4;--i){
+     cout << i << " ";
+   }
+   for(int i=sz/4;i>=1;--i){
+     cout << i << " ";
+   }
+   cout << "\n";
+   for(int i=sz/4+1; i<=3*sz/4;++i) cout << i << " ";
+   cout << "\n";
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=1;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
