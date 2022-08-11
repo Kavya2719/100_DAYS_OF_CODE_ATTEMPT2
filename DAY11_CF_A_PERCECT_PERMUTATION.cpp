2749a3a06c368c0b33afee3d228ccfc1cacc83b1
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-08-12 00:12:30 +0530

    Create DAY11_CF_A_PERCECT_PERMUTATION.cpp

---
+++ DAY11_CF_A_PERCECT_PERMUTATION.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+typedef long long ll;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   int sz=t;
+   cin >> sz;
+   
+   cout << sz << " ";
+   for(int i=1;i<sz;i++) cout << i << " ";
+   cout << "\n";
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=10;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
