3a242464c544282a3b4f21c7c64bcca849edf292
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-08-15 23:56:04 +0530

    Create DAY15_CC_SUBTAKS.cpp

---
+++ DAY15_CC_SUBTAKS.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+typedef long long ll;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   int sz,m,k;
+   cin >> sz >> m >> k;
+   
+   int ct=0;
+   bool f=0;
+   for(int i=0;i<sz;++i){
+     int val; cin >> val;
+     if(val) ct++;
+     else ct=-1*inf;
+     if(ct>=m) f=1;
+   }
+   
+   if(ct==sz) cout << "100\n";
+   else if(f) cout << k << "\n";
+   else cout << "0\n";
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=1;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
