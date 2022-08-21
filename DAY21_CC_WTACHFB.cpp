f8cc113206b338a0288d5f7e8b3654f9185b15c0
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-08-21 23:48:42 +0530

    Create DAY21_CC_WTACHFB.cpp

---
+++ DAY21_CC_WTACHFB.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+typedef long long ll;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   int sz;
+   cin >> sz;
+   
+   int mx=0 ,mn=inf;
+   while(sz--){
+     int a,b,c;
+     cin >> a >> b >> c;
+     if(a==1){
+       yes;
+       mx = max(b,c); mn=min(b,c);
+     }else{
+       if(b==c){
+         mx=mn=b; yes;
+       }
+       else if(min(b,c) < mx){ mx=max(c,b); mn=min(b,c); yes; }
+       else no;
+     }
+   }
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=1;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
