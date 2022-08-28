8cc4027c7f79bde66c006197f07ee7b8fada8206
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-08-29 00:22:39 +0530

    Create DAY28_CC_PRGIFT.cpp

---
+++ DAY28_CC_PRGIFT.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+typedef long long ll;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   int sz,k;
+   cin >> sz >> k;
+   
+   int odd=0,tmp=k;
+   for(int i=0;i<sz;++i){
+     int val; cin >> val;
+     if(val&1) odd++;
+     else k--;
+   }
+   
+   if(k<=0 && tmp) yes;
+   else if((!tmp) && odd) yes;
+   else no;
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=1;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
