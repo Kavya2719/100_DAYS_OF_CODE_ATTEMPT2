ae28a7ce685d9a1067381d3a2dd5cc8f41969e6b
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-09-17 01:42:38 +0530

    Create DAY47_CC_ODDPAIRS.cpp

---
+++ DAY47_CC_ODDPAIRS.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define bipc(x) __builtin_popcountll(x)
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+using ll = long long;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   ll sz;
+   cin >> sz;
+   
+   ll eve = sz/2;
+   ll a = sz;
+   
+   sz*=sz;
+   sz -= eve*eve;
+   
+   ll odd = a - eve;
+   sz -= odd*odd;
+   
+   cout << sz << "\n";
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=1;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
