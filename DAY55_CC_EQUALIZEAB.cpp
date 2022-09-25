97b5687ff8333dfeb74c918bf7ce73d64033799b
Kavya Gupta <95200924+Kavya2719@users.noreply.github.com>
2022-09-26 00:03:46 +0530

    Create DAY55_CC_EQUALIZEAB.cpp

---
+++ DAY55_CC_EQUALIZEAB.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define iofast() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define bipc(x) __builtin_popcountll(x)
+
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define ccase(t) "Case #" << t << ": "
+//#define lp(i,x,sz) for(int i=x;i<sz;i++)
+using ll = long long;
+
+const int N = (int)2e5 + 10;
+const int inf = (int)1e9+10;
+const int M = (int)1e9 + 7;
+
+
+
+void RadheRadhe(int t,bool kavya2719 = 1){
+   int a,b,x;
+   cin >> a >> b >> x;
+   
+   a = abs(a-b);
+   x*=2;
+   if(a%x) no;
+   else yes;
+}
+
+
+void phle_ka_kaam(){
+   
+}
+
+
+int main()
+{
+    iofast();
+    phle_ka_kaam();
+    
+    int TTTT=1;
+    cin >> TTTT;
+    for(int i=1;i<=TTTT;i++)
+       RadheRadhe(i);
+}
